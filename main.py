8adf7d002fcbbf4fa0a9929725f98ff1ec148866
SuperDragon777 <Vadim.muromtsev@gmail.com>
2026-02-03 11:44:58 +0300

    init

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import os
+import random
 
+bal = 100
+matcoin = 0
+
+while True:
+    os.system("clear")
+    print(f"Ваш баланс: {bal}")
+    print(f"Ваши маткоины: {matcoin}")
+    
+    print("\nЧто вы хотите сделать?")
+    print("1. Ничего")
+    
+    userinput = input("> ")
